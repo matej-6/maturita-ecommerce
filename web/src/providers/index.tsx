f82543434d1b82a687c360e24970e24493ee089b
Matej Barca <matejbarca2006@gmail.com>
2025-11-26 20:15:47 +0100

    change theme to light

--- web/src/providers/index.tsx
+++ web/src/providers/index.tsx
@@ -6,7 +6,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <I18nProvider>
       <QueryProvider>
-        <Toaster richColors theme="dark" />
+        <Toaster richColors />
         {children}
       </QueryProvider>
     </I18nProvider>
